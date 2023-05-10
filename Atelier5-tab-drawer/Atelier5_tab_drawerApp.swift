fcfbb8f1bc91c618f43f56b5c4c2a59e46296d74
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-10 15:41:08 +0100

    Initial Commit

---
+++ Atelier5-tab-drawer/Atelier5_tab_drawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atelier5_tab_drawerApp.swift
+//  Atelier5-tab-drawer
+//
+//  Created by CHEBIHI FAYCAL on 10/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Atelier5_tab_drawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Atelier5-tab-drawer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Atelier5-tab-drawer
+//
+//  Created by CHEBIHI FAYCAL on 10/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
